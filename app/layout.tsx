56656ea1f49bcc193211ce69e2e85a01e9c3d3ab
APHIWAT ON-AT <changsaoggo123@gmail.com>
2026-06-02 14:53:28 +0700

    feat: add site assets and configure root layout with metadata and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "portfolio-apiwat",
   description: "Web profile",
+  icons: {
+    icon: "/logo.png",
+  },
 };
 
 export default function RootLayout({
